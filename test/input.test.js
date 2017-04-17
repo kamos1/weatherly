51cb1d052cabbc55a0bc11ff02e5e61bca7b7ebc
kamos1 <keji.amos@gmail.com>
2017-04-17 16:31:29 -0600

    testing happened

--- test/input.test.js
+++ test/input.test.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Input from '../lib/components/Input'
 import $ from 'jquery'
 
 
-it('state.input should change when input is changed', () => {
-  var mock = jest.fn()
-  var wrapper = shallow(<Input submit={()=>{mock}} />)
-  // var input = wrapper.find('div');  				// css selectors
-  var input = wrapper.find('input[type="text"]');  	// css selectors
-  var btn = wrapper.find('#submitButton');   		// id selector
-  var input = wrapper.find('#mainInput'); 		// class selectors
+it('should update state.location when submit is clicked', () => {
+  const mock = jest.fn();
+  const wrapper = shallow(<Input getWeather={() => { mock }}/>);
+  const input = wrapper.find('input[type="text"]');
+  const btn = wrapper.find('#submitButton');
 
   input.simulate('change', { target: { value: 'oreos' } });
+  expect(wrapper.state('location')).toEqual('oreos');
   btn.simulate('click');
-
-  expect(wrapper.state('input')).toEqual('oreos')
+  expect(wrapper.state('location')).toEqual('');
 });
+
+it('should call componentDidMount', () => {
+    let spy = jest.spyOn(Input.prototype, 'componentDidMount')
+    let wrapper = mount(<Input getWeather={() => {}} />)
+
+    expect(spy).toHaveBeenCalled();
+});
+
+it('should call submit', () => {
+  let spy = jest.spyOn(Input.prototype, 'submit')
+  let wrapper = mount(<Input getWeather={() => {}} />)
+  let button = wrapper.find('#submitButton')
+
+  button.simulate('click');
+
+  expect(spy).toHaveBeenCalled();
+})
