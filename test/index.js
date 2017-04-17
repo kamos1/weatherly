abe315158e8f5fe6858322612e7957e2ee6799e3
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-04-17 13:09:55 -0600

    Responsive styling

---
+++ test/index.js
@@ -0,0 +1,16 @@
+// import Input from '../lib/components/Input';
+// import React from 'react';
+// import { shallow, mount } from 'enzyme';
+//
+// describe('Input: ', () => {
+//   it('should change state when a user enters an input', () => {
+//     const wrapper = shallow(<Input submit={() => {}} />);
+//     const input = wrapper.find('#mainInput');
+//     const submitButton = wrapper.find('#submit');
+//
+//     input.simulate('change', { target: { value: 'denver' } });
+//     submitButton.simulate('click');
+//
+//     expect(wrapper.state('input')).toEqual('denver');
+//   });
+// });
